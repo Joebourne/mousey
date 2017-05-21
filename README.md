49294b3aff566bd73fa997bb93c79421ac84dfb6
Joe Bourne <joe@aspokesmansaid.com>
2017-05-21 13:40:49 +0100

    feat: Translate the point on the circumference to CSS properties of the inner circle

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mousey
+
+mousey is a project to track mouse movement and represent this movement on the circumference of a circle.
+
+Using right-angled triangles, the horizontal and vertical distances of the mouse from the centre of the viewport are trigonometrically equated to a smaller triangle whose hypotenuse is the radius of the outer circle.
+
+We can thus calculate the position of the inner circle (that sits on the circumference) and update these properties in the DOM whenever the mouse is moved.
+
+If the viewport is resized, the midpoint is recalculated.
+
+Click the background for a bit of variety.
--- mousey.js
+++ mousey.js
@@ -1,33 +1,47 @@
-document.addEventListener('click', function(e) {
+document.addEventListener('mousemove', function(e) {
     mousePoint.x = e.clientX;
     mousePoint.y = resolution.y - e.clientY;
     var angle = findAngle(resolutionMidpoint, mousePoint);
-    console.log('angle: ' + angle);
-    var xOnCircle = getXOnCircle(angle, 150);
-    console.log('xOnCircle: ' + xOnCircle);
-    var yOnCircle = getYOnCircle(angle, 150);
-    console.log('yOnCircle: ' + yOnCircle);
+    var xOnCircle = getXOnCircle(angle, radius);
+    var yOnCircle = getYOnCircle(angle, radius);
+    moveInnerCircle(xOnCircle, yOnCircle);
+});
+
+document.addEventListener('mousedown', function() {
+    document.getElementById('background').classList.add('clicked');
+});
+
+document.addEventListener('mouseup', function() {
+    document.getElementById('background').classList.remove('clicked');
 });
 
 window.addEventListener('resize', function() {
-    getResolutionMidpoint();
+    getResolutionAndMidpoint();
 });
 
 var resolution = {};
+var radius;
 var resolutionMidpoint = {};
 var mousePoint = {};
+
+getResolutionAndMidpoint();
 
 function getResolutionAndMidpoint() {
     resolution.x = window.innerWidth;
     resolution.y = window.innerHeight;
     resolutionMidpoint.x = resolution.x / 2;
     resolutionMidpoint.y = resolution.y / 2;
-    console.log(resolutionMidpoint);
 }
 
-getResolutionAndMidpoint();
+function moveInnerCircle(xOnCircle, yOnCircle) {
+    var innerCircle = document.getElementById('inner-circle');
+    var circle = document.getElementById('circle');
+    var diameterString = window.getComputedStyle(circle).width;
+    radius = diameterString.slice(0, diameterString.length - 2) / 2;
+    innerCircle.style.setProperty('top', (radius - yOnCircle) + 'px');
+    innerCircle.style.setProperty('right', (radius - xOnCircle) + 'px');
+}
 
-// wrongly assuming mousepoint from bottom up, when it's actually top down
 function findAngle(center, mouse) {
     var x = mouse.x - center.x;
     var y = mouse.y - center.y;
